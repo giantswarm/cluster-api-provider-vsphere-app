15f7761a5198abfbb271aceba0266eec57c51e8a
Erkan Erol <erkanerol92@gmail.com>
2024-06-04 19:54:35 +0300

    Bump version from 1.8.9 to 1.9.3 (#97)

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -5,7 +5,7 @@ SHELL := /bin/bash
 APPLICATION_NAME="cluster-api-provider-vsphere"
 
 UPSTREAM_ORG="kubernetes-sigs"
-TAG_TO_SYNC="v1.8.9"
+TAG_TO_SYNC="v1.9.3"
 
 OS ?= $(shell go env GOOS 2>/dev/null || echo linux)
 ARCH ?= $(shell go env GOARCH 2>/dev/null || echo amd64)
